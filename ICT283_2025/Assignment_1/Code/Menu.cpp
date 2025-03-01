87cb4c562bcc1bc3a1034ef12e36f21583dfe2ee
Aithusa712 <kimandrew712@gmail.com>
2025-03-01 21:55:37 +0800

    more comments, revisions, comments and comments, yipee

--- ICT283_2025/Assignment_1/Code/Menu.cpp
+++ ICT283_2025/Assignment_1/Code/Menu.cpp
@@ -148,11 +148,16 @@ void Menu::HandleMonthlySpeed(const SensorLog &sensor_data) const {
 
   // Condition to check if the month and year contains any data, if it does,
   // calculate and prompt results
+  // The "S" in the ValidateMonth parameter stands for Speed
+
   if (ValidateMonth(sensor_data, month_int, year_int, "S")) {
 
+    // Call functions from the Calculator class
     average = calculate.AverageSpeed(sensor_data, month_int, year_int);
     standard_deviation =
         calculate.StdDevSpeed(sensor_data, average, month_int, year_int);
+
+    // Print Results
     cout << month << " " << year_int << ":" << endl
          << "Average Speed: " << average << "km/h" << endl
          << "Sample stdev: " << standard_deviation << endl;
@@ -163,14 +168,26 @@ void Menu::HandleMonthlySpeed(const SensorLog &sensor_data) const {
   }
 }
 
+// Handle the monthly temperature prompt
+
 void Menu::HandleMonthlyTemperature(const SensorLog &sensor_data) const {
+
+  // initialize local variables
+
   Calculator calculate;
   int month_int = 0;
   int year_int = 0;
   float average = 0.0f;
   float standard_deviation = 0.0f;
   bool month_exist = false;
+
+  // Call GetValidYear routine to prompt a user for a Year and validate if its a
+  // numerical string or out of range
+
   year_int = GetValidYear();
+
+  // Loop through the sensor data vector struct to find if there is any data on
+  // the year the user entered
 
   for (int index = 0; index < sensor_data.size(); index++) {
     if (sensor_data[index].date.GetYear() == year_int) {
@@ -178,62 +195,121 @@ void Menu::HandleMonthlyTemperature(const SensorLog &sensor_data) const {
       break;
     }
   }
+
+  // If month exist for that year
+
   if (month_exist) {
+
+    // Print the year
     cout << endl << year_int << ": " << endl;
+
+    // Loop through all the months of a year
+
     for (int month_index = 1; month_index < MAX_MONTHS; month_index++) {
+
+      // month_index = the month to be validated
+
       month_int = month_index;
+
+      // Check if there is data for the current month index
+      // The "T" inside the ValidateMonth parameter stands for temperature
+
       if (ValidateMonth(sensor_data, month_int, year_int, "T")) {
+
+        // Call functions from the Calculator class
         average =
             calculate.AverageTemperature(sensor_data, month_int, year_int);
         standard_deviation = calculate.StdDevTemperature(sensor_data, average,
                                                          month_int, year_int);
-
+        // Print Results
         cout << int_to_month(month_int) << ":"
              << " average: " << average
              << " degrees C, Sample stdev: " << standard_deviation << endl;
-      } else {
+      }
+      // If the month has no Data
+      else {
         cout << int_to_month(month_int) << ": No Data" << endl;
       }
     }
-  } else {
+  }
+  // If entire year has no Data
+  else {
     cout << year_int << ": No data";
   }
 }
 
+// Handle the monthly Solar Radiation prompt
+
 void Menu::HandleMonthlySolarRadiation(const SensorLog &sensor_data) const {
+
+  // Initialize local variables
+
   Calculator calculate;
   int month_int = 0;
   int year_int = 0;
   bool month_exist = false;
   float total_solar_radiation = 0;
 
+  // Call GetValidYear routine to prompt a user for a Year and validate if its a
+  // numerical string or out of range
+
   year_int = GetValidYear();
+
+  // Loop through the sensor data vector struct to find if there is any data on
+  // the year the user entered
+
   for (int index = 0; index < sensor_data.size(); index++) {
     if (sensor_data[index].date.GetYear() == year_int) {
       month_exist = true;
       break;
     }
   }
+
+  // If month exists for that year
+
   if (month_exist) {
+    // Print the year
     cout << endl << year_int << ": " << endl;
+
+    // Loop through all the months
     for (int month_index = 1; month_index < MAX_MONTHS; month_index++) {
+
+      // month_index = the month to be validated
+
       month_int = month_index;
+
+      // Check if there is data for the current month index
+      // The "SR" inside the ValidateMonth parameter stands for Solar Radiation
+
       if (ValidateMonth(sensor_data, month_int, year_int, "SR")) {
+
+        // Call function from the Calculator Class
+
         total_solar_radiation =
             calculate.TotalSolarRadiation(sensor_data, month_int, year_int);
 
+        // Print Results
+
         cout << int_to_month(month_int) << ": " << total_solar_radiation
              << " kWh/m^2 " << endl;
-      } else {
+      }
+      // If the month has no Data
+      else {
         cout << int_to_month(month_int) << ": No Data" << endl;
       }
     }
-  } else {
+  }
+  // If the entire year has no Data
+  else {
     cout << year_int << ": No data";
   }
 }
 
+// Handle All Data and print to WindTempSolar.csv
+
 void Menu::HandleAllData(const SensorLog &sensor_data) const {
+
+  // Initialize local variables
 
   Calculator calculate;
   FileHandler handleFile;
@@ -247,32 +323,64 @@ void Menu::HandleAllData(const SensorLog &sensor_data) const {
   int year_int = 0;
   bool month_exist = false;
 
+  // Call GetValidYear routine to prompt a user for a Year and validate if its a
+  // numerical string or out of range
+
   year_int = GetValidYear();
+
+  // Loop through the sensor data vector struct to find if there is any data on
+  // the year the user entered
   for (int index = 0; index < sensor_data.size(); index++) {
     if (sensor_data[index].date.GetYear() == year_int) {
       month_exist = true;
     }
   }
 
+  // Check if there is data for the current month index
+  // The "SR" inside the ValidateMonth parameter stands for Solar Radiation
+
   if (month_exist) {
+
+    // Print year to output stream
+
     output << year_int << ": " << endl;
+
+    // Loop through all the months
+
     for (int month_index = 1; month_index < MAX_MONTHS; month_index++) {
+
+      // month_index = the current month to calculate results for
 
       month_int = month_index;
 
+      // empty data counter - if empty data = 0, that means there is no data for
+      // the entire month
+
       int empty_data = 0;
 
+      // Call functions from the calculate class
+
+      // Average Speed
       average_speed = calculate.AverageSpeed(sensor_data, month_int, year_int);
+      // Sample Standard Deviation Speed
       stdev_speed = calculate.StdDevSpeed(sensor_data, average_speed, month_int,
                                           year_int);
+      // Average Temperature
       average_temperature =
           calculate.AverageTemperature(sensor_data, month_int, year_int);
+      // Sample Standard Deviation Temperature
       stdev_temperature = calculate.StdDevTemperature(
           sensor_data, average_temperature, month_int, year_int);
+      // Total solar Radiation
       total_solar_radiation =
           calculate.TotalSolarRadiation(sensor_data, month_int, year_int);
 
+      // Print month name to output stream
+
       output << int_to_month(month_int);
+
+      // Check if value is empty, if it is add 1 to empty_data, if not print
+      // result to output stream
 
       if (average_speed != 0) {
         output << "," << average_speed;
@@ -300,6 +408,7 @@ void Menu::HandleAllData(const SensorLog &sensor_data) const {
       } else {
         empty_data++;
       }
+      // If empty data is equal to 5 that means there is no data available
       if (empty_data == 5) {
         output << ": No Data";
       }
@@ -308,12 +417,24 @@ void Menu::HandleAllData(const SensorLog &sensor_data) const {
     }
   }
 
+  // Call FileHandler class, send the filename to insert the output stream into
+
   handleFile.writeToFile("WindTempSolar.csv", output.str());
 }
 
+// Boolean function to Validate if month exists
+
 bool Menu::ValidateMonth(const SensorLog &sensor_data, const int month,
                          const int year, const string sensor_type) const {
+
+  // Check which sensor_type was given in the parameter
+  // S for Speed
+  // T for Temperature
+  // SR for Solar Radiation
+
   if (sensor_type == "S") {
+    // Loop through all sensor records and return true if a speed record exists
+    // for the year and month that was given in the parameter
     for (int index = 0; index < sensor_data.size(); index++) {
       if (sensor_data[index].date.GetMonth() == month &&
           sensor_data[index].date.GetYear() == year &&
@@ -323,6 +444,8 @@ bool Menu::ValidateMonth(const SensorLog &sensor_data, const int month,
     }
   }
   if (sensor_type == "T") {
+    // Loop through all sensor records and return true if a temperature record
+    // exists for the year and month that was given in the parameter
     for (int index = 0; index < sensor_data.size(); index++) {
       if (sensor_data[index].date.GetMonth() == month &&
           sensor_data[index].date.GetYear() == year &&
@@ -332,6 +455,8 @@ bool Menu::ValidateMonth(const SensorLog &sensor_data, const int month,
     }
   }
   if (sensor_type == "SR") {
+    // Loop through all sensor records and return true if a Solar Radiation
+    // record exists for the year and month that was given in the parameter
     for (int index = 0; index < sensor_data.size(); index++) {
       if (sensor_data[index].date.GetMonth() == month &&
           sensor_data[index].date.GetYear() == year &&
@@ -340,46 +465,54 @@ bool Menu::ValidateMonth(const SensorLog &sensor_data, const int month,
       }
     }
   }
-
-  if (sensor_type == "ALL") {
-    for (int index = 0; index < sensor_data.size(); index++) {
-      if (sensor_data[index].date.GetMonth() == month &&
-          sensor_data[index].date.GetYear() == year) {
-        return true;
-      }
-    }
-  }
-
+  // Else return false
   return false;
 }
 
+// Prompt User for an input with validation
 int Menu::GetValidYear() const {
+
+  // Initialize local variables
   bool valid_year;
   string year_string;
   int year_int;
+
+  // Break loop if year is validated, else, repeat
   while (!valid_year) {
+
+    // Prompt User for year
     cout << "\nEnter Year: ";
     cin >> year_string;
 
     try {
-
+      // Loop through all the char of the string and check if it's a digit
       for (char c : year_string) {
         if (!isdigit(c)) {
+          // Throw a string exception
           throw year_string;
         }
       }
+      // If all characters are numerical, convert string to integer
       year_int = stoi(year_string);
 
+      // Check if user input is within range
       if (year_int < 2005 || year_int > 2025) {
+        // Throw a integer exception
         throw year_int;
-      } else {
+      }
+      // year is validated, assign valid_year = true to exit loop
+      else {
         valid_year = true;
       }
-    } catch (const string &invalid_argument) {
+    }
+    // Catch any string exception
+    catch (const string &invalid_argument) {
       cout << "Error: Please enter numeric digits only for the year.\n\n"
            << endl;
       cin.clear();
-    } catch (const int &year_out_of_range) {
+    }
+    // Catch any integer exception
+    catch (const int &year_out_of_range) {
       cout << "Please enter a year between 2007 and 2025." << endl;
     }
   }

4678b454bef3306e73642622dae39899389d52b1
Aithusa712 <kimandrew712@gmail.com>
2025-03-25 18:48:19 +0800

    converted BST into AVL tree to reduce insertion time. AVL code taken from lab notes

--- ICT283_2025/Assignment_2/Assignment_2/Bst.h
+++ ICT283_2025/Assignment_2/Assignment_2/Bst.h
@@ -15,10 +15,12 @@ template <typename T> struct Node {
   T info;
   Node *left;
   Node *right;
+  int bfactor;
   Node(T newInfo) {
     info = newInfo;
     left = nullptr;
     right = nullptr;
+    bfactor = 1;
   }
 };
 
@@ -115,38 +117,204 @@ public:
 
 protected:
   Node<T> *root; /// holds the root node of the tree
+  void rotateToLeft(Node<T>* &root);
+  void rotateToRight(Node<T>* &root);
+  void balanceFromLeft(Node<T>* &root);
+  void balanceFromRight(Node<T>* &root);
+  void insertIntoBst(Node<T>* &root, Node<T> *newNode, bool& isTaller);
 };
 
 template <typename T> Bst<T>::Bst() { root = nullptr; }
 
 template <typename T> Bst<T>::~Bst() { Delete(root); }
 
-template <typename T> void Bst<T>::Insert(T newData) {
-  Node<T> *newNode = new Node<T>(newData);
-  Node<T> *current;
-  Node<T> *trailCurrent;
+/// Start of AVL STUFF
+
+
+template <typename T>
+void Bst<T>::rotateToLeft(Node<T>* &root) {
   if (root == nullptr) {
-    root = newNode;
-    return;
+    std::cout << "Error in the tree" << std::endl;
+  } else if (root->right == nullptr) {
+    std::cout << "Error in the tree: No right subtree to rotate." << std::endl;
   } else {
-    current = root;
-  }
-  while (current != nullptr) {
-    trailCurrent = current;
-    if (current->info == newData) {
-      return;
-    } else if (current->info > newData) {
-      current = current->left;
-    } else {
-      current = current->right;
-    }
-  }
-  if (trailCurrent->info > newData) {
-    trailCurrent->left = newNode;
-  } else {
-    trailCurrent->right = newNode;
+    Node<T> *p = root->right;
+    root->right = p->left;
+    p->left = root;
+    root = p;
   }
 }
+
+template <typename T>
+void Bst<T>::rotateToRight(Node<T>* &root) {
+  if (root == nullptr) {
+    std::cout << "Error in the tree" << std::endl;
+  } else if (root->left == nullptr) {
+    std::cout << "Error in the tree: No left subtree to rotate." << std::endl;
+  } else {
+    Node<T> *p = root->left;
+    root->left = p->right;
+    p->right = root;
+    root = p;
+  }
+}
+
+template <typename T>
+void Bst<T>::balanceFromLeft(Node<T>* &root) {
+  Node<T> *p = root->left;
+  Node<T> *w;
+  switch (p->bfactor) {
+    case -1:
+      root->bfactor = 0;
+      p->bfactor = 0;
+      rotateToRight(root);
+      break;
+    case 1:
+      w = p->right;
+      switch (w->bfactor) {
+        case -1:
+          root->bfactor = 1;
+          p->bfactor = 0;
+          break;
+        case 0:
+          root->bfactor = 0;
+          p->bfactor = 0;
+          break;
+        case 1:
+          root->bfactor = 0;
+          p->bfactor = -1;
+          break;
+      }
+      w->bfactor = 0;
+      rotateToLeft(p);
+      root->left = p;
+      rotateToRight(root);
+      break;
+    default:
+      std::cout << "Error: Cannot balance from the left." << std::endl;
+      break;
+  }
+}
+
+template <typename T>
+void Bst<T>::balanceFromRight(Node<T>* &root) {
+  Node<T> *p = root->right;
+  Node<T> *w;
+  switch (p->bfactor) {
+    case 1:
+      root->bfactor = 0;
+      p->bfactor = 0;
+      rotateToLeft(root);
+      break;
+    case -1:
+      w = p->left;
+      switch (w->bfactor) {
+        case -1:
+          root->bfactor = 0;
+          p->bfactor = 1;
+          break;
+        case 0:
+          root->bfactor = 0;
+          p->bfactor = 0;
+          break;
+        case 1:
+          root->bfactor = -1;
+          p->bfactor = 0;
+          break;
+      }
+      w->bfactor = 0;
+      rotateToRight(p);
+      root->right = p;
+      rotateToLeft(root);
+      break;
+    default:
+      std::cout << "Error: Cannot balance from the right." << std::endl;
+      break;
+  }
+}
+
+template <typename T>
+void Bst<T>::insertIntoBst(Node<T>* &root, Node<T> *newNode, bool& isTaller) {
+  if (root == nullptr) {
+    root = newNode;
+    isTaller = true;
+  } else if (root->info == newNode->info) {
+    std::cout << "No duplicates are allowed." << std::endl;
+  } else if (root->info > newNode->info) {
+    insertIntoBst(root->left, newNode, isTaller);
+    if (isTaller) {
+      switch (root->bfactor) {
+        case -1:
+          balanceFromLeft(root);
+          isTaller = false;
+          break;
+        case 0:
+          root->bfactor = -1;
+          isTaller = true;
+          break;
+        case 1:
+          root->bfactor = 0;
+          isTaller = false;
+          break;
+      }
+    }
+  } else {
+    insertIntoBst(root->right, newNode, isTaller);
+    if (isTaller) {
+      switch (root->bfactor) {
+        case -1:
+          root->bfactor = 0;
+          isTaller = false;
+          break;
+        case 0:
+          root->bfactor = 1;
+          isTaller = true;
+          break;
+        case 1:
+          balanceFromRight(root);
+          isTaller = false;
+          break;
+      }
+    }
+  }
+}
+
+template <typename T>
+void Bst<T>::Insert(T newData) {
+  bool isTaller = false;
+  Node<T> *newNode = new Node<T>(newData);
+  insertIntoBst(root, newNode, isTaller);
+}
+
+
+/// END OF AVL STUFF
+
+/*template <typename T> void Bst<T>::Insert(T newData) {*/
+/*  Node<T> *newNode = new Node<T>(newData);*/
+/*  Node<T> *current;*/
+/*  Node<T> *trailCurrent;*/
+/*  if (root == nullptr) {*/
+/*    root = newNode;*/
+/*    return;*/
+/*  } else {*/
+/*    current = root;*/
+/*  }*/
+/*  while (current != nullptr) {*/
+/*    trailCurrent = current;*/
+/*    if (current->info == newData) {*/
+/*      return;*/
+/*    } else if (current->info > newData) {*/
+/*      current = current->left;*/
+/*    } else {*/
+/*      current = current->right;*/
+/*    }*/
+/*  }*/
+/*  if (trailCurrent->info > newData) {*/
+/*    trailCurrent->left = newNode;*/
+/*  } else {*/
+/*    trailCurrent->right = newNode;*/
+/*  }*/
+/*}*/
 template <typename T> void Bst<T>::Delete(Node<T> *&root) {
   if (root == nullptr) {
     return;
